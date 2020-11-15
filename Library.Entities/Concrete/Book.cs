fbd56149274ecff355bd57271e16a3c11460acef
Zafer CÖMERT <zafer.comert@hotmail.com>
2020-11-15 08:56:24 +0300

    The first layer named as Entities has been added to the project.

---
+++ Library.Entities/Concrete/Book.cs
@@ -0,0 +1,18 @@
+﻿using Library.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Entities.Concrete
+{
+    public class Book : IEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Year { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
