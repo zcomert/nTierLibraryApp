97e3338f2ad045182cdd85e3d9b7be04fbc01da5
Zafer CÖMERT <zafer.comert@hotmail.com>
2020-11-19 09:06:14 +0300

    Business logic has been added.

--- Library.ASPNET/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Library.ASPNET.Global" Language="C#" %>
---
+++ Library.DataAccess/Concrete/EntitiyFramework/EfCategoryDal.cs
@@ -0,0 +1,18 @@
+﻿using Library.DataAccess.Abstract;
+using Library.DataAccess.Concrete.EntitiyFramework;
+using Library.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Concrete
+{
+    public class EfCategoryDal : 
+        EfRepositoryBase<Category, LibraryDBContext>,
+        ICategoryDal
+    {
+
+    }
+}
